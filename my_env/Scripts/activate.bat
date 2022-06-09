2b6395328f0b2028227c3b32dd0e95c06b0ab64a
alexandra <alexandra_juice@mail.ru>
2022-06-09 12:06:41 +0300

    finally

--- my_env/Scripts/activate.bat
+++ my_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Projects\pop-_ezha\my_env
+set VIRTUAL_ENV=C:\project\sokolova1\pop-_ezha\my_env
 
 if not defined PROMPT set PROMPT=$P$G
 
